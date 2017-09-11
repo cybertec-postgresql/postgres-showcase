14603bb27852bd970c719143d27700aad1fd1802
Kaarel Moppel <kaarel.moppel@gmail.com>
2017-09-12 00:58:13 +0300

    Stored procedures - added recommendations on using pure SQL functions

--- 20_stored_procedures.sql
+++ 20_stored_procedures.sql
@@ -2,18 +2,25 @@
 SET ROLE TO demorole;
 
 /*
-PL/pgSQL is a specialized programming language that allows to query data and provides common features like variables, comparisons, 
-branching (if/else), error handling, calling other functions.
+PL/pgSQL is a specialized programming language that allows to query data and
+provides common features like variables, comparisons, branching (if/else),
+error handling, calling other functions. It is a procedural language and should
+be chosen when SQL (being an imperative language) cannot solve the problem at
+hand - in particular since executing queries with SQL functions can be
+optimized moreso by the planner than queries with PL/pgSQL functions.
 
-Stored procedures in PL/pgSQL are beneficial as they provide layering for the "data logic" (one can do modifications 
-without applications knowing about it) and usually also improve performance for multi-step data processing scenarios.
+Stored procedures in PL/pgSQL are beneficial as they provide layering for the
+"data logic" (one can do modifications without applications knowing about it)
+and usually also improve performance for multi-step data processing scenarios.
 
-In PL/pgSQL there's no distinction between a function (restricted to returning a single scalar value in Oracle PL/SQL) and a procedure, 
-all stored procedural code are 'functions' that can return anything (scalars, multiple values, rows of multiple values or table types) 
-or nothing.
+In PL/pgSQL there's no distinction between a function (restricted to returning
+a single scalar value in Oracle PL/SQL) and a procedure, all stored procedural
+code are 'functions' that can return anything (scalars, multiple values, rows
+of multiple values or table types) or nothing.
 
-NB! In addition to PL/pgSQL there are dozens of other PL-languages available for installation for more complex data processing needs - 
-plpython, plperl, pljava to name a few.
+NB! In addition to PL/pgSQL there are dozens of other PL-languages available
+for installation for more complex data processing needs - plpython, plperl,
+pljava to name a few.
 */
 
 -- a simple sproc returning a scalar
@@ -26,7 +33,11 @@ $SQL$
 LANGUAGE plpgsql;
 
 
--- note that above functions we could (and should) write in pure SQL as this has some performance benefits
+/*
+Note that the above simple function we could (and should) write as a "pure"-SQL
+function as this has possible performance benefits due to not needing a context
+switch by the execution engine.
+*/
 CREATE OR REPLACE FUNCTION say_hello_2() RETURNS text as
 $SQL$
     SELECT 'Hello! Can you hear me?'::text;
@@ -38,25 +49,31 @@ LANGUAGE sql;
 CREATE OR REPLACE FUNCTION perform_transaction(
     account_from        account.account_id%TYPE,
     account_to          account.account_id%TYPE,
-    amount_to_transfer  account.balance%TYPE)
+    amount              account.balance%TYPE)
 RETURNS VOID AS
 $SQL$
 DECLARE
   acccount_balance account.balance%TYPE;
 BEGIN
   
-  SELECT balance INTO acccount_balance FROM account WHERE account_id = account_from;
+  SELECT
+    balance INTO acccount_balance
+  FROM
+    account
+  WHERE
+    account_id = account_from;
   
   IF NOT FOUND THEN
     RAISE EXCEPTION 'Account % not found', account_from;
   END IF;
   
-  IF acccount_balance < amount_to_transfer THEN
+  IF acccount_balance < amount THEN
     RAISE EXCEPTION 'Not enough funds';
   END IF;
 
-  UPDATE account SET balance = balance + amount_to_transfer WHERE account_id = account_to;      -- NB! Very simplistic approch, lot of problems possible here
-  UPDATE account SET balance = balance - amount_to_transfer WHERE account_id = account_from;
+  -- NB! Very simplistic approch, lot of problems possible here
+  UPDATE account SET balance = balance + amount WHERE account_id = account_to;
+  UPDATE account SET balance = balance - amount WHERE account_id = account_from;
 
 END;
 $SQL$
